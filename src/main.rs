03bb72e22e402555e1720262e9a2d838de30599f
Maxim Baz <git@maximbaz.com>
2021-05-10 21:18:56 +0200

    Accept bind address and port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use clap::{App, Arg};
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fmt;
+use std::net::SocketAddr;
 use std::process::Command;
 use warp::Filter;
 
@@ -22,15 +24,43 @@ impl fmt::Display for IP {
 
 #[tokio::main]
 async fn main() {
+    let matches = App::new("iptables-exporter")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("address")
+                .short("a")
+                .long("address")
+                .value_name("address")
+                .help("Bind address")
+                .default_value("127.0.0.1")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .value_name("port")
+                .help("Bind port")
+                .default_value("9119")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let address = matches.value_of("address").unwrap_or("127.0.0.1");
+    let port = matches.value_of("port").unwrap_or("9119");
+
+    let bind = format!("{}:{}", address, port);
+    let bind = bind
+        .parse::<SocketAddr>()
+        .expect(&format!("Unable to parse bind address: {}", &bind));
+
     let shutdown = async {
         tokio::signal::ctrl_c()
             .await
             .expect("failed to listen for Ctrl+C");
     };
-
     let metrics = warp::any().map(metrics_endpoint);
-    let (_, server) =
-        warp::serve(metrics).bind_with_graceful_shutdown(([172, 17, 0, 1], 9119), shutdown);
+    let (_, server) = warp::serve(metrics).bind_with_graceful_shutdown(bind, shutdown);
 
     server.await;
 }
