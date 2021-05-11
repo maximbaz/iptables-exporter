3f8c541fc211bd038a6c60df2c0c364b5c637eb6
Maxim Baz <git@maximbaz.com>
2021-05-12 01:48:50 +0200

    Add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use std::process::Command;
 use strum::IntoEnumIterator;
 use strum_macros::{Display, EnumIter};
 
-#[derive(Clone, Display, EnumIter, Eq, Hash, PartialEq)]
+#[derive(Clone, Debug, Display, EnumIter, Eq, Hash, PartialEq)]
 enum IP {
     #[strum(serialize = "4")]
     IPv4,
@@ -16,7 +16,7 @@ enum IP {
     IPv6,
 }
 
-#[derive(Clone, Display, EnumIter, Eq, Hash, PartialEq)]
+#[derive(Clone, Debug, Display, EnumIter, Eq, Hash, PartialEq)]
 #[strum(serialize_all = "snake_case")]
 enum Table {
     Filter,
@@ -26,6 +26,7 @@ enum Table {
     Security,
 }
 
+#[derive(Debug)]
 struct Rule {
     name: String,
     ip: IP,
@@ -33,9 +34,10 @@ struct Rule {
     stats: Stats,
 }
 
+#[derive(Debug)]
 struct Stats {
-    packets: u64,
-    bytes: u64,
+    packets: u128,
+    bytes: u128,
 }
 
 impl PartialEq for Rule {
@@ -55,7 +57,7 @@ impl Hash for Rule {
 }
 
 impl Rule {
-    fn add_stats(self, other: &Self) -> Self {
+    fn merge(self, other: &Self) -> Self {
         Self {
             ip: self.ip,
             table: self.table,
@@ -76,10 +78,12 @@ pub fn metrics_endpoint() -> String {
             .fold(HashSet::new(), |mut acc, val| {
                 acc.insert(match acc.get(&val) {
                     None => val,
-                    Some(other) => val.add_stats(other),
+                    Some(other) => val.merge(other),
                 });
                 acc
-            }),
+            })
+            .into_iter()
+            .collect(),
     )
 }
 
@@ -123,7 +127,7 @@ fn parse_stats(ip: &IP, table: &Table, stats: String) -> Vec<Rule> {
         .collect_vec()
 }
 
-fn format_metrics(data: HashSet<Rule>) -> String {
+fn format_metrics(data: Vec<Rule>) -> String {
     let line = |measure, rule: &Rule, value| {
         format!(
             r#"iptables_{}{{ip_version="{}",table="{}",rule="{}"}} {}"#,
@@ -148,4 +152,131 @@ fn format_metrics(data: HashSet<Rule>) -> String {
             .map(|rule| line("bytes", rule, rule.stats.bytes))
             .join("\n"),
     )
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn rule(ip: IP, table: Table, name: &str, packets: u128, bytes: u128) -> Rule {
+        Rule {
+            ip,
+            table,
+            name: name.to_string(),
+            stats: Stats { packets, bytes },
+        }
+    }
+
+    #[test]
+    fn test_parse_stats_ipv4() {
+        let input = "
+Chain DOCKER-USER (1 references)
+    pkts      bytes target     prot opt in     out     source               destination
+    1149   238269 ACCEPT     all  --  *      *       0.0.0.0/0            0.0.0.0/0            ctstate RELATED
+35169773 6943845843 ACCEPT     all  --  *      *       0.0.0.0/0            0.0.0.0/0            ctstate ESTABLISHED /* iptables-exporter established */
+     254    11521 DROP       all  --  *      *       0.0.0.0/0            0.0.0.0/0            ctstate INVALID
+      89     5328            tcp  --  *      *       0.0.0.0/0            0.0.0.0/0            tcp dpt:22 flags:0x17/0x02 ctstate NEW
+    1185    66020            tcp  --  *      *       0.0.0.0/0            0.0.0.0/0            tcp dpt:53 flags:0x17/0x02 ctstate NEW /* iptables-exporter dns */
+  270644 20134629            udp  --  *      *       0.0.0.0/0            0.0.0.0/0            udp dpt:53 /* iptables-exporter dns */
+  493870 33326724 RETURN     all  --  *      *       0.0.0.0/0            0.0.0.0/0
+
+Chain ICMP (4 references)
+    pkts      bytes target     prot opt in     out     source               destination
+    1269   102634 ACCEPT     icmp --  *      *       0.0.0.0/0            0.0.0.0/0            limit: up to 10/sec burst 5 mode srcip /* iptables-exporter icmp */
+       0        0 DROP       all  --  *      *       0.0.0.0/0            0.0.0.0/0            /* iptables-exporter drop icmp */
+".to_string();
+
+        let expected = vec![
+            rule(IP::IPv4, Table::Filter, "established", 35169773, 6943845843),
+            rule(IP::IPv4, Table::Filter, "dns", 1185, 66020),
+            rule(IP::IPv4, Table::Filter, "dns", 270644, 20134629),
+            rule(IP::IPv4, Table::Filter, "icmp", 1269, 102634),
+            rule(IP::IPv4, Table::Filter, "drop icmp", 0, 0),
+        ];
+        assert_eq!(expected, parse_stats(&IP::IPv4, &Table::Filter, input));
+    }
+
+    #[test]
+    fn test_parse_stats_ipv6() {
+        let input = "
+Chain INPUT (policy DROP 17 packets, 2511 bytes)
+    pkts      bytes target     prot opt in     out     source               destination
+     670    69680 ACCEPT     all      lo     *       ::/0                 ::/0
+   50330 14370092 ACCEPT     all      *      *       ::/0                 ::/0                 ctstate RELATED,ESTABLISHED /* iptables-exporter related */
+      65     5196 DROP       all      *      *       ::/0                 ::/0                 ctstate INVALID
+    1899   140244 ACCEPT     tcp      *      *       ::/0                 ::/0                 tcp dpt:53 flags:0x17/0x02 ctstate NEW /* iptables-exporter dns */
+   91342  8504843 ACCEPT     udp      *      *       ::/0                 ::/0                 multiport dports 53 /* iptables-exporter dns */
+
+Chain OUTPUT (policy ACCEPT 227941 packets, 57081722 bytes)
+    pkts      bytes target     prot opt in     out     source               destination
+".to_string();
+
+        let expected = vec![
+            rule(IP::IPv6, Table::Filter, "related", 50330, 14370092),
+            rule(IP::IPv6, Table::Filter, "dns", 1899, 140244),
+            rule(IP::IPv6, Table::Filter, "dns", 91342, 8504843),
+        ];
+        assert_eq!(expected, parse_stats(&IP::IPv6, &Table::Filter, input));
+    }
+
+    #[test]
+    fn test_rule_eq() {
+        assert_eq!(
+            true,
+            rule(IP::IPv4, Table::Nat, "aaa", 1, 1).eq(&rule(IP::IPv4, Table::Nat, "aaa", 2, 3))
+        );
+
+        assert_eq!(
+            false,
+            rule(IP::IPv4, Table::Nat, "aaa", 1, 1).eq(&rule(IP::IPv6, Table::Nat, "aaa", 1, 1))
+        );
+
+        assert_eq!(
+            false,
+            rule(IP::IPv4, Table::Nat, "aaa", 1, 1).eq(&rule(IP::IPv4, Table::Filter, "aaa", 1, 1))
+        );
+
+        assert_eq!(
+            false,
+            rule(IP::IPv4, Table::Nat, "aaa", 1, 1).eq(&rule(IP::IPv4, Table::Nat, "bbb", 1, 1))
+        );
+    }
+
+    #[test]
+    fn test_rule_merge() {
+        assert_eq!(
+            rule(IP::IPv4, Table::Mangle, "aaa", 100, 200),
+            rule(IP::IPv4, Table::Mangle, "aaa", 50, 120).merge(&rule(
+                IP::IPv4,
+                Table::Mangle,
+                "aaa",
+                50,
+                80
+            ))
+        );
+    }
+
+    #[test]
+    fn test_format_metrics() {
+        let expected = r#"
+# HELP iptables_packets Number of matched packets
+# TYPE iptables_packets counter
+iptables_packets{ip_version="4",table="filter",rule="aaa"} 10
+iptables_packets{ip_version="4",table="raw",rule="bbb"} 2000
+iptables_packets{ip_version="6",table="mangle",rule="ccc"} 0
+
+# HELP iptables_bytes Number of matched bytes
+# TYPE iptables_bytes counter
+iptables_bytes{ip_version="4",table="filter",rule="aaa"} 70
+iptables_bytes{ip_version="4",table="raw",rule="bbb"} 7000
+iptables_bytes{ip_version="6",table="mangle",rule="ccc"} 17
+"#;
+        let rules = vec![
+            rule(IP::IPv4, Table::Filter, "aaa", 10, 70),
+            rule(IP::IPv4, Table::Raw, "bbb", 2000, 7000),
+            rule(IP::IPv6, Table::Mangle, "ccc", 0, 17),
+        ];
+
+        assert_eq!(expected, format_metrics(rules));
+    }
 }
